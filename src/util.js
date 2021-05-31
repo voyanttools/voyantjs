e44342ac75a92893a324a81c478a0f8cf9f9956f
Andrew James <ajmacdonald@users.noreply.github.com>
2021-05-31 16:44:18 -0400

    feat(util): add methods for determining value type

--- src/util.js
+++ src/util.js
@@ -46,6 +46,38 @@ class Util {
 		return before + '<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M16.59 8.59L12 13.17 7.41 8.59 6 10l6 6 6-6z"/><path d="M0 0h24v24H0z" fill="none"/></svg>'+more.substring(0,500)+' <a href="">+</a><div style="display: none">'+more.substring(501)+'</div>' + after;
 
 	}
+
+	static isString(val) {
+		return typeof val === 'string';
+	}
+
+	static isNumber(val) {
+		return typeof val === 'number';
+	}
+
+	static isBoolean(val) {
+		return typeof val === 'boolean';
+	}
+
+	static isUndefined(val) {
+		return typeof val === 'undefined';
+	}
+
+	static isArray(val) {
+		return Object.prototype.toString.call(val) === '[object Array]';
+	}
+
+	static isObject(val) {
+		return Object.prototype.toString.call(val) === '[object Object]';
+	}
+
+	static isNull(val) {
+		return Object.prototype.toString.call(val) === '[object Null]';
+	}
+
+	static isElement(val) {
+		return val instanceof Element;
+	}
 }
 
 export default Util;
